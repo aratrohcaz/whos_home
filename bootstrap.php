bd552ba83aac144274e88ad1d57eb88c016da65c
aratrohcaz <thataddress@protonmail.com>
2017-12-10 18:26:57 +1030

    Intialising Doctrine CLI and Bootstrap
    Update git-ignore file
    Add bootstrap class for loading settings and other pre-flight functions
    Add cli-config to calling doctrine in the terminal
    Add database.yml example
    Add symlink to doctrine
    Add gitkeep file for log directory

---
+++ bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+## Bootstrap class for including all autoloaders and inialising settings
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Yaml\Parser;
+
+$dev_mode = getenv('doc_env', true) ? true : false;
+
+$yml_parser = new Parser();
+
+$paths = ["/config"];
+$config = Setup::createYAMLMetadataConfiguration($paths, $dev_mode);
+$db_params = $yml_parser->parse( file_get_contents(__DIR__."/config/database.yml"));
+
+$entity_manager = EntityManager::create($db_params, $config);
